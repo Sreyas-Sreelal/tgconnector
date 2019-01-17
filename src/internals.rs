79e08c45c80f3fdbbe3965bb9527370c4144c8c4
Sreyas <sreyassreelal@gmail.com>
2019-01-17 06:23:15 +0000

    tidy up

--- src/internals.rs
+++ src/internals.rs
@@ -8,37 +8,37 @@ pub fn clear_caches(cache: &mut std::collections::LinkedList<String>) {
 
 pub fn update_process(plugin: &mut super::TgConnector) {
 	for (id,bot) in &plugin.bots {
-			for update in bot.update_reciever.as_ref().unwrap().try_iter() {
-				let results = update.result.unwrap();
+		for update in bot.update_reciever.as_ref().unwrap().try_iter() {
+			let results = update.result.unwrap();
 
-				for result in results {
-					if result.message.text != None {
-						plugin.telegram_messages.push_front(result.message.text.unwrap());
-						plugin.telegram_chatid.push_front(result.message.chat.id);
-						plugin.telegram_chattype.push_front(result.message.chat.chat_type);
+			for result in results {
+				if result.message.text != None {
+					plugin.telegram_messages.push_front(result.message.text.unwrap());
+					plugin.telegram_chatid.push_front(result.message.chat.id);
+					plugin.telegram_chattype.push_front(result.message.chat.chat_type);
 
-						match result.message.from.username {
-							Some(username) => {
-								plugin.telegram_username.push_front(username);
-							}
-							None => {
+					match result.message.from.username {
+						Some(username) => {
+							plugin.telegram_username.push_front(username);
+						}
+						None => {
 
-							}
-						};
+						}
+					};
 
-						match result.message.chat.title {
-							Some(chatname) => {
-								plugin.telegram_chatname.push_front(chatname);
-							}
-							None => {
-								
-							}
-						};                     
+					match result.message.chat.title {
+						Some(chatname) => {
+							plugin.telegram_chatname.push_front(chatname);
+						}
+						None => {
+							
+						}
+					};                     
 
-						callbacks::on_tg_message(plugin,id.clone(), result.message.from.id.clone());
-					}
+					callbacks::on_tg_message(plugin,id.clone(), result.message.from.id.clone());
 				}
 			}
+		}
 			
 		}
 }
