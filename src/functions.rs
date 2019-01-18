da4194932c3ff06a7c349ea3fa085d7731e3029c
Sreyas <sreyassreelal@gmail.com>
2019-01-18 04:00:13 +0000

    skip serialization if parse_mode in None

--- src/functions.rs
+++ src/functions.rs
@@ -8,5 +8,6 @@ pub struct SendMessage {
 	pub chat_id: String,
 	pub text: String,
 	pub reply_to_message_id: Option<i32>,
+	 #[serde(skip_serializing_if = "Option::is_none")]
 	pub parse_mode: Option<&'static str>,	
 }
