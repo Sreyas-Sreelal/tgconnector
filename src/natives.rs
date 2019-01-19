2f7607b298eb3a75955fc8836940155fb3dd00aa
Sreyas <sreyassreelal@gmail.com>
2019-01-19 05:15:11 +0000

    cleanup

--- src/natives.rs
+++ src/natives.rs
@@ -7,198 +7,198 @@ use functions::*;
 
 impl super::TgConnector {
 	pub fn bot_connect(&mut self,_amx:&AMX,token:String) -> AmxResult<Cell> {
-        let api = BOT::new(token);
-        if api.connect() {
-            self.bots.insert(self.bot_context_id,api);
-            self.bot_context_id += 1;
-            Ok(self.bot_context_id as Cell -1)
-        }else {
-            Ok(-1)
-        }
-    }
+		let api = BOT::new(token);
+		if api.connect() {
+			self.bots.insert(self.bot_context_id,api);
+			self.bot_context_id += 1;
+			Ok(self.bot_context_id as Cell -1)
+		}else {
+			Ok(-1)
+		}
+	}
 
-    pub fn bot_connect_from_env(&mut self,_amx:&AMX,variable:String) -> AmxResult<Cell> {
-        let token = std::env::var_os(&variable);
-        if token == None {
-            log!("**[TGConnector] Error environment variable {:?} is not set",variable);
-            Ok(-1)
-        }else {
-            let token = token.unwrap().into_string().unwrap();
-            let api = BOT::new(token);
+	pub fn bot_connect_from_env(&mut self,_amx:&AMX,variable:String) -> AmxResult<Cell> {
+		let token = std::env::var_os(&variable);
+		if token == None {
+			log!("**[TGConnector] Error environment variable {:?} is not set",variable);
+			Ok(-1)
+		}else {
+			let token = token.unwrap().into_string().unwrap();
+			let api = BOT::new(token);
 
-            if api.connect() {
-                self.bots.insert(self.bot_context_id,api);
-                self.bot_context_id += 1;
-                Ok(self.bot_context_id as Cell -1)
-            }else {
-                Ok(-1)
-            }
-        }
-    }
+			if api.connect() {
+				self.bots.insert(self.bot_context_id,api);
+				self.bot_context_id += 1;
+				Ok(self.bot_context_id as Cell -1)
+			}else {
+				Ok(-1)
+			}
+		}
+	}
 
-    pub fn bot_send_message(&mut self,_amx:&AMX,botid:usize,chatid:String,text:String,reply_id:i32,parse_mode:i32,callback:String) -> AmxResult<Cell> {
-        let reply: Option<i32>;
-        if reply_id == -1 {
-            reply = None;
-        }else {
-            reply = Some(reply_id);
-        }
+	pub fn bot_send_message(&mut self,_amx:&AMX,botid:usize,chatid:String,text:String,reply_id:i32,parse_mode:i32,callback:String) -> AmxResult<Cell> {
+		let reply: Option<i32>;
+		if reply_id == -1 {
+			reply = None;
+		}else {
+			reply = Some(reply_id);
+		}
 
-        let parsemode:Option<&str> = match parse_mode {
-            0 => Some("HTML"),
-            1 => Some("markdown"),
-            _ => None
-        };
+		let parsemode:Option<&str> = match parse_mode {
+			0 => Some("HTML"),
+			1 => Some("markdown"),
+			_ => None
+		};
 
-        let callback:Option<String> = match callback.is_empty() {
-            true => None,
-            false => Some(callback),
-        };
+		let callback:Option<String> = match callback.is_empty() {
+			true => None,
+			false => Some(callback),
+		};
 
-        if !self.bots.contains_key(&botid) {
-            log!("**[TGConnector] Error Invalid bot id {} passed",botid);
-            Ok(0)
-        }else {
-            let send_message_obj = SendMessage {
+		if !self.bots.contains_key(&botid) {
+			log!("**[TGConnector] Error Invalid bot id {} passed",botid);
+			Ok(0)
+		}else {
+			let send_message_obj = SendMessage {
 				chat_id: chatid,
 				text: text,
 				reply_to_message_id: reply,
 				parse_mode: parsemode
-		    };
-            self.bots[&botid].send_message(send_message_obj,callback);
-            Ok(1)
-        }
-    }
+			};
+			self.bots[&botid].send_message(send_message_obj,callback);
+			Ok(1)
+		}
+	}
 
-    pub fn bot_delete_message(&mut self,_amx:&AMX,botid:usize,chatid:String,messageid:i32) -> AmxResult<Cell> {
-        if !self.bots.contains_key(&botid) {
-            log!("**[TGConnector] Error Invalid bot id {} passed",botid);
-            Ok(0)
-        }else {
-            let delete_message_obj = DeleteMessage {
+	pub fn bot_delete_message(&mut self,_amx:&AMX,botid:usize,chatid:String,messageid:i32) -> AmxResult<Cell> {
+		if !self.bots.contains_key(&botid) {
+			log!("**[TGConnector] Error Invalid bot id {} passed",botid);
+			Ok(0)
+		}else {
+			let delete_message_obj = DeleteMessage {
 				chat_id: chatid,
 				message_id: messageid,
-		    };
-            self.bots[&botid].delete_message(delete_message_obj);
-            Ok(1)
-        }
-    }
+			};
+			self.bots[&botid].delete_message(delete_message_obj);
+			Ok(1)
+		}
+	}
 
-    pub fn bot_edit_message(&mut self,_amx:&AMX,botid:usize,chatid:String,messageid:i32,text:String,parse_mode:i32)-> AmxResult<Cell> {
-        let parsemode:Option<&str> = match parse_mode {
-            0 => Some("HTML"),
-            1 => Some("markdown"),
-            _ => None
-        };
+	pub fn bot_edit_message(&mut self,_amx:&AMX,botid:usize,chatid:String,messageid:i32,text:String,parse_mode:i32)-> AmxResult<Cell> {
+		let parsemode:Option<&str> = match parse_mode {
+			0 => Some("HTML"),
+			1 => Some("markdown"),
+			_ => None
+		};
 
-        if !self.bots.contains_key(&botid) {
-            log!("**[TGConnector] Error Invalid bot id {} passed",botid);
-            Ok(0)
-        }else {
-            let edit_message_obj = EditMessageText {
+		if !self.bots.contains_key(&botid) {
+			log!("**[TGConnector] Error Invalid bot id {} passed",botid);
+			Ok(0)
+		}else {
+			let edit_message_obj = EditMessageText {
 				chat_id: chatid,
 				text: text,
-                message_id: messageid,
+				message_id: messageid,
 				parse_mode: parsemode
-		    };
-            self.bots[&botid].edit_message(edit_message_obj);
-            Ok(1)
-        }
-    }
+			};
+			self.bots[&botid].edit_message(edit_message_obj);
+			Ok(1)
+		}
+	}
 
-    pub fn get_message(&mut self,_amx:&AMX,dest:&mut Cell,size:usize) -> AmxResult<Cell> {
-        let string = self.telegram_messages.front();
+	pub fn get_message(&mut self,_amx:&AMX,dest:&mut Cell,size:usize) -> AmxResult<Cell> {
+		let string = self.telegram_messages.front();
 
-        if string != None {
-            match encode_replace(&string.unwrap()) {
-                Ok(encoded) => {
-                    set_string!(encoded,dest,size);
-                    Ok(1)
-                },
-                Err(err) => {
-                    log!("**[TGConnector][get_message] Failed encoding {:?} \n {:?}",string.unwrap(),err);
-                    Ok(0)
-                }   
-            }
-        }else {
-             
-            Ok(0)
-        }
-    }
+		if string != None {
+			match encode_replace(&string.unwrap()) {
+				Ok(encoded) => {
+					set_string!(encoded,dest,size);
+					Ok(1)
+				},
+				Err(err) => {
+					log!("**[TGConnector][get_message] Failed encoding {:?} \n {:?}",string.unwrap(),err);
+					Ok(0)
+				}   
+			}
+		}else {
+			 
+			Ok(0)
+		}
+	}
 
-    pub fn get_username(&mut self,_amx:&AMX,dest:&mut Cell,size:usize) -> AmxResult<Cell> {
-        let string = self.telegram_username.front();
-        
-        if string != None {
-            match encode_replace(&string.unwrap()) {
-                Ok(encoded) => {
-                    set_string!(encoded,dest,size);
-                    Ok(1)
-                },
-                Err(err) => {
-                    log!("**[TGConnector][get_username] Failed encoding {:?} \n {:?}",string.unwrap(),err);
-                    Ok(0)
-                }   
-            }
-        }else {   
-            Ok(0)
-        }
-    }
-    
-    pub fn get_chatid(&mut self,_amx:&AMX,dest:&mut Cell,size:usize) -> AmxResult<Cell> {
-        let string = self.telegram_chatid.front();
+	pub fn get_username(&mut self,_amx:&AMX,dest:&mut Cell,size:usize) -> AmxResult<Cell> {
+		let string = self.telegram_username.front();
+		
+		if string != None {
+			match encode_replace(&string.unwrap()) {
+				Ok(encoded) => {
+					set_string!(encoded,dest,size);
+					Ok(1)
+				},
+				Err(err) => {
+					log!("**[TGConnector][get_username] Failed encoding {:?} \n {:?}",string.unwrap(),err);
+					Ok(0)
+				}   
+			}
+		}else {   
+			Ok(0)
+		}
+	}
+	
+	pub fn get_chatid(&mut self,_amx:&AMX,dest:&mut Cell,size:usize) -> AmxResult<Cell> {
+		let string = self.telegram_chatid.front();
 
-        if string != None {
-            match encode_replace(&string.unwrap()) {
-                Ok(encoded) => {
-                    set_string!(encoded,dest,size);
-                    Ok(1)
-                },
-                Err(err) => {
-                    log!("**[TGConnector][get_chatid] Failed encoding {:?} \n {:?}",string.unwrap(),err);
-                    Ok(0)
-                }   
-            }
-        }else {   
-            Ok(0)
-        }
-    }
+		if string != None {
+			match encode_replace(&string.unwrap()) {
+				Ok(encoded) => {
+					set_string!(encoded,dest,size);
+					Ok(1)
+				},
+				Err(err) => {
+					log!("**[TGConnector][get_chatid] Failed encoding {:?} \n {:?}",string.unwrap(),err);
+					Ok(0)
+				}   
+			}
+		}else {   
+			Ok(0)
+		}
+	}
 
-    pub fn get_chatname(&mut self,_amx:&AMX,dest:&mut Cell,size:usize) -> AmxResult<Cell> {
-       let string =  self.telegram_chatname.front();
-       
-       if string != None {
-            match encode_replace(&string.unwrap()) {
-                Ok(encoded) => {
-                    set_string!(encoded,dest,size);
-                    Ok(1)
-                },
-                Err(err) => {
-                    log!("**[TGConnector][get_chatname] Failed encoding {:?} \n {:?}",string.unwrap(),err);
-                    Ok(0)
-                }   
-            }
-        }else {   
-            Ok(0)
-        }
-    }
+	pub fn get_chatname(&mut self,_amx:&AMX,dest:&mut Cell,size:usize) -> AmxResult<Cell> {
+	   let string =  self.telegram_chatname.front();
+	   
+	   if string != None {
+			match encode_replace(&string.unwrap()) {
+				Ok(encoded) => {
+					set_string!(encoded,dest,size);
+					Ok(1)
+				},
+				Err(err) => {
+					log!("**[TGConnector][get_chatname] Failed encoding {:?} \n {:?}",string.unwrap(),err);
+					Ok(0)
+				}   
+			}
+		}else {   
+			Ok(0)
+		}
+	}
 
-    pub fn get_chattype(&mut self,_amx:&AMX,dest:&mut Cell,size:usize) -> AmxResult<Cell> {
-        let string = self.telegram_chattype.front();
-        
-        if string != None {
-            match encode_replace(&string.unwrap()) {
-                Ok(encoded) => {
-                    set_string!(encoded,dest,size);
-                    Ok(1)
-                },
-                Err(err) => {
-                    log!("**[TGConnector][get_chattype] Failed encoding {:?} \n {:?}",string.unwrap(),err);
-                    Ok(0)
-                }   
-            }
-        }else {   
-            Ok(0)
-        }
-    }
+	pub fn get_chattype(&mut self,_amx:&AMX,dest:&mut Cell,size:usize) -> AmxResult<Cell> {
+		let string = self.telegram_chattype.front();
+		
+		if string != None {
+			match encode_replace(&string.unwrap()) {
+				Ok(encoded) => {
+					set_string!(encoded,dest,size);
+					Ok(1)
+				},
+				Err(err) => {
+					log!("**[TGConnector][get_chattype] Failed encoding {:?} \n {:?}",string.unwrap(),err);
+					Ok(0)
+				}   
+			}
+		}else {   
+			Ok(0)
+		}
+	}
 }
