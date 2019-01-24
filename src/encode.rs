33c74a9e4cfa57809cd1822de4427f7292a27e61
Sreyas <sreyassreelal@gmail.com>
2019-01-24 08:02:11 +0000

    editor config set to tabsize 4

--- src/encode.rs
+++ src/encode.rs
@@ -4,5 +4,5 @@ use encoding::{Encoding, EncoderTrap};
 
 //Just a modification to encode function to replace the chararcter on error instead of giving up entire thing
 pub fn encode_replace(string: &str) -> AmxResult<Vec<u8>> {
-    WINDOWS_1251.encode(string, EncoderTrap::Replace).map_err(|_| AmxError::Format)
+	WINDOWS_1251.encode(string, EncoderTrap::Replace).map_err(|_| AmxError::Format)
 }
