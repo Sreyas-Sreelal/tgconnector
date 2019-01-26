0f35dde06efd2d92d970592164c3a05826b96114
Sreyas <sreyassreelal@gmail.com>
2019-01-26 10:17:11 +0000

    reduce repeated pattern'

--- src/macros.rs
+++ src/macros.rs
@@ -1,17 +1,11 @@
 #[macro_export]
 macro_rules! exec_callback {
-	($amx:ident, $name:ident; $($args:tt)*) => {
+	($amx:ident, $name:tt; $($args:tt)*) => {
 		{
 			$amx.find_public(&$name)
 				.and_then(|index| exec!($amx, index; $($args)*))
 		}
 	};
-	($amx:ident, $name:expr; $($args:tt)*) => {
-        {
-            $amx.find_public($name)
-                .and_then(|index| exec!($amx, index; $($args)*))
-        }
-    };
 }
 
 macro_rules! cast_amx {
