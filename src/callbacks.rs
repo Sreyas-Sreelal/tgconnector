0259ebc571efa7478abf9b71b3272a6f6345078c
Sreyas <sreyassreelal@gmail.com>
2019-01-27 03:45:54 +0000

    place macros in one place

--- src/callbacks.rs
+++ src/callbacks.rs
@@ -1,26 +1,5 @@
 use samp_sdk::amx::AMX;
 
-macro_rules! execute {
-    ($amx_list:ident,$name:tt,$botid:ident;$($args:tt)*) => {
-        let mut executed: bool = false;
-        for amx in $amx_list {
-            let amx = cast_amx!(amx);
-            let botid: usize = *$botid;
-            match exec_callback!(amx,$name;botid,$($args)*) {
-                Ok(_) => {
-                    executed = true;
-                }
-                Err(_err) => {
-                    continue;
-                }
-            }
-        }
-        if !executed {
-            log!("**[TGConnector] Error executing callback {}",$name);
-        }
-    };
-}
-
 pub fn on_tg_message(amx_list: &Vec<usize>, botid: &usize, fromid: i32, message_id: i32) {
     execute!(amx_list,"OnTGMessage",botid;fromid,message_id);
 }
--- src/macros.rs
+++ src/macros.rs
@@ -1,4 +1,26 @@
 #[macro_export]
+
+macro_rules! execute {
+    ($amx_list:ident,$name:tt,$botid:ident;$($args:tt)*) => {
+        let mut executed: bool = false;
+        for amx in $amx_list {
+            let amx = cast_amx!(amx);
+            let botid: usize = *$botid;
+            match exec_callback!(amx,$name;botid,$($args)*) {
+                Ok(_) => {
+                    executed = true;
+                }
+                Err(_err) => {
+                    continue;
+                }
+            }
+        }
+        if !executed {
+            log!("**[TGConnector] Error executing callback {}",$name);
+        }
+    };
+}
+
 macro_rules! exec_callback {
 	($amx:ident, $name:tt; $($args:tt)*) => {
 		{
