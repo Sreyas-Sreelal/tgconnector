2c65797e0261616b58f87b0a863c36b27299bfd5
SyS <sreyassreelal@gmail.com>
2020-04-15 12:39:30 +0530

    support for proxy #2

--- src/http.rs
+++ src/http.rs
@@ -10,6 +10,7 @@ pub struct HttpRequest {
     pub url: String,
     pub method: HttpMethod,
     pub body: Option<String>,
+    pub proxy_url: Option<String>,
 }
 
 impl HttpRequest {
@@ -22,7 +23,18 @@ impl HttpRequest {
             }
         };
         let method = match self.method {
-            HttpMethod::Get => requests_obj.get(),
+            HttpMethod::Get => {
+                if let Some(proxy_url) = &self.proxy_url {
+                    match requests_obj.proxy(&proxy_url) {
+                        Ok(method) => method.get(),
+                        Err(err) => {
+                            return Err(format!("Error connecting to proxy server \n{:?}", err));
+                        }
+                    }
+                } else {
+                    requests_obj.get()
+                }
+            }
 
             HttpMethod::Post => {
                 let body = &self.body.clone().unwrap();
@@ -30,10 +42,18 @@ impl HttpRequest {
                 let mut headers = HashMap::new();
                 headers.insert("Content-Type".to_string(), "application/json".to_string());
                 requests_obj.headers(headers);
-                requests_obj.post()
+                if let Some(proxy_url) = &self.proxy_url {
+                    match requests_obj.proxy(&proxy_url) {
+                        Ok(method) => method.post(),
+                        Err(err) => {
+                            return Err(format!("Error connecting to proxy server \n{:?}", err));
+                        }
+                    }
+                } else {
+                    requests_obj.post()
+                }
             }
         };
-
         match method.send() {
             Ok(data) => Ok(data.text()),
 
