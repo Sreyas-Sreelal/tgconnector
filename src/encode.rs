141bfb3f9e8682a5f868f775431a2db5250b6f8d
SyS <sreyassreelal@gmail.com>
2023-04-18 21:57:10 +0530

    attempting utf8 encoding

--- src/encode.rs
+++ src/encode.rs
@@ -1,13 +1,10 @@
-use encoding::all::WINDOWS_1251;
+use encoding::all::UTF_8;
 use encoding::{EncoderTrap, Encoding};
+use std::error;
 use std::str::from_utf8;
 
-pub fn encode_replace(string: &str) -> Result<String, ()> {
-    match WINDOWS_1251.encode(string, EncoderTrap::Replace) {
-        Ok(bytes) => match from_utf8(&bytes) {
-            Ok(data) => Ok(String::from(data)),
-            Err(_) => Err(()),
-        },
-        Err(_) => Err(()),
-    }
+pub fn encode_replace(string: &str) -> Result<String, Box<dyn error::Error>> {
+    let bytes =  UTF_8.encode(string, EncoderTrap::Replace)?;
+    let data = from_utf8(&bytes)?; 
+    Ok(data.to_string())    
 }
